fd3103fd09306cd56d0b8bd429cb19e19c740da7
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-08-30 02:50:01 +0530

    Inventory update

--- server/index.js
+++ server/index.js
@@ -1,11 +1,38 @@
-require("dotenv").config();
+//server.js
+require('dotenv').config()
+
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const stripe = require("stripe")('<API KEY>');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
-app.listen(8000, () => {
-  console.log("http://localhost:8000");
+const calculateTotalOrderAmount = (items) => {
+	return items[0].amount * 100;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+	const { items } = req.body;
+
+	const paymentIntent = await stripe.paymentIntents.create({
+		amount: calculateTotalOrderAmount(items),
+		currency: "usd",
+		description: "This is for GFG Stripe API Demo",
+		automatic_payment_methods: {
+			enabled: true,
+		},
+	});
+
+	res.send({
+		clientSecret: paymentIntent.client_secret,
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server started on PORT ${PORT}`);
 });
